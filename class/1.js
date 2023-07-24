0ad30fe6d364ea4cdf9ebbdcb57ed3cc6b5dd7f2
Aman Maddheshiya <developeramanyt@gmail.com>
2023-07-24 09:16:23 +0100

    first commit

---
+++ class/1.js
@@ -0,0 +1,61 @@
+console.log('Hello everyone');
+
+// <!-- variables, constants and comments  -->
+const store=34;   
+
+// declaration
+console.log(store); //redcleration
+// console.log(store);
+//  var Rahul=19;
+//  console.log(Rahul);
+ // reassign
+
+
+ // constants
+
+ const value=10;
+ console.log(value);
+ //Data types of values
+
+ // 7 data types
+
+ //1.  Numbers 1,2,55 3.4
+ // string "I am string", "34"
+
+ // Boolean true/false
+ // null  Intentionally absence of values 
+
+ let score=null;
+ console.log(score);
+
+ // undefined variable declared but not initialized
+
+ // object complex data structure
+ // symbols looks like object
+
+ // strings
+ console.log('Hello Coders');
+
+ let firstName='full stack'
+ let lastName='DEVELOPER'
+ console.log(firstName,lastName);
+
+ // string concatenation
+
+ // method using +  operator
+
+//  let fullName= firstName + " " + lastName;
+//  console.log(fullName);
+
+ // method 2 template literals
+  let fullName=`I want to become ${firstName} ${lastName}`;
+  console.log(fullName);
+
+  // string characters
+  console.log(firstName[5]);
+
+  // string methods
+   console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+
+console.log(typeof firstName);
